3eabe685a787e4804c3835a446b9c101c1883972
Dilip Kr. Shukla <fusionstrings@gmail.com>
2022-03-27 23:01:17 +0200

    use react default export

--- server.jsx
+++ server.jsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
+import React from 'react';
 import { serve } from "https://deno.land/std@0.132.0/http/server.ts";
 
 console.log("http://localhost:8000/");
-serve((req) => new Response(`Hello World\n ${React}`), { port: 8000 });
+serve((req) => new Response(`Hello World\n ${React.version}`), { port: 8000 });
